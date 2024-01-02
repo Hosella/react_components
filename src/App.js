55ff0db03c5cf470beba6b98b426a1458fbb1653
Sonia <sonya221005@gmail.com>
2024-01-02 14:16:29 +0200

    Created component Multiplication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Multiplication } from "./components/Multiplication";
 import { Post } from "./components/Post";
 
 function App() {
   return (
     <div className="App">
       <Post 
-        // title='title'
         data='2022-05-15'
-        // text='text'
+      />
+
+      <Multiplication 
+        a={3}
+        b={6}
       />
     </div>
   );
---
+++ src/components/Multiplication/index.jsx
@@ -0,0 +1,5 @@
+export const Multiplication = ({ a = 1, b = 1 }) => {
+  return (
+    <span>{`Multiplication of ${a} and ${b} is ${a * b}`}</span>
+  );
+}
